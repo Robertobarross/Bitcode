8341e23b39c83adc65243e8255cf846e7e8df93d
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-06-17 19:55:08 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+// Estilizando App.js
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#696969',
+      alignItems: 'center',
+      justifyContent: 'center', 
+    },
+
+  });
+
+export default styles
