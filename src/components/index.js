31b5fe807efaf2b92aec79694209737412a9feb4
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-06-17 19:53:31 -0300

    Create index.js

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native-web';
+
+export default function Title(){
+    return(
+        <View>
+            <Text>ONEBITHEALTH</Text>
+        </View>
+    );
+}
