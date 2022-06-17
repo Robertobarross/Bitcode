16d127fc7d67d290294b4e5f02540d726b3d4dce
Roberto Barrosss <robertobarros27esp@gmail.com>
2022-06-17 19:54:54 -0300

    Create style.js

---
+++ src/Form/style.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+// Estilizando o formulário
+const styles = StyleSheet.create({
+    form: {
+      flex: 1,
+      backgroundColor: '#DCDCDC',
+      width: 300,
+      top: 100,
+      borderRadius: 30,
+      alignItems: 'center',
+      justifyContent: 'center', 
+    },
+
+  });
+
+export default styles
